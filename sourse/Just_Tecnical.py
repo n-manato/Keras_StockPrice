56d871f6596dfac108fb02184d3332ee72a84f51
n-manato <131611846+n-manato@users.noreply.github.com>
2024-02-01 20:02:37 +0900

    2024020102

--- sourse/Just_Tecnical.py
+++ sourse/Just_Tecnical.py
@@ -10,7 +10,7 @@ start = dt.date(2020, 1, 1)
 end = dt.date(2023, 1, 1)
 
 # データ取得
-ticker = 'TSLA'
+ticker = 'GS'
 data = yf.download(ticker, start=start, end=end)
 
 # カラムの名前を変更
@@ -21,9 +21,11 @@ data['SMA'] = talib.SMA(data['Close'], timeperiod=14)
 data['EMA'] = talib.EMA(data['Close'], timeperiod=14)
 data['RSI'] = talib.RSI(data['Close'], timeperiod=14)
 data['MOM'] = talib.MOM(data['Close'], timeperiod=14)
-macd, signal, _ = talib.MACD(data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+macd, signal, _ = talib.MACD(
+    data['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
 data['MACD'] = macd
-data['ATR'] = talib.ATR(data['High'], data['Low'], data['Close'], timeperiod=14)
+data['ATR'] = talib.ATR(data['High'], data['Low'],
+                        data['Close'], timeperiod=14)
 dx = talib.DX(data['High'], data['Low'], data['Close'])
 data['DX'] = dx
 data.dropna(inplace=True)
@@ -39,37 +41,43 @@ axs[0, 0].set_xlabel('日付')
 axs[0, 0].set_ylabel('価格')
 axs[0, 0].grid()
 
-axs[0, 1].plot(data.index, data['SMA'], label='SMA', linewidth=0.7, color='orange')
+axs[0, 1].plot(data.index, data['SMA'], label='SMA',
+               linewidth=0.7, color='orange')
 axs[0, 1].set_title('SMA')
 axs[0, 1].set_xlabel('日付')
 axs[0, 1].set_ylabel('価格')
 axs[0, 1].grid()
 
-axs[1, 0].plot(data.index, data['EMA'], label='EMA', linewidth=0.7, color='green')
+axs[1, 0].plot(data.index, data['EMA'], label='EMA',
+               linewidth=0.7, color='green')
 axs[1, 0].set_title('EMA')
 axs[1, 0].set_xlabel('日付')
 axs[1, 0].set_ylabel('価格')
 axs[1, 0].grid()
 
-axs[1, 1].plot(data.index, data['RSI'], label='RSI', linewidth=0.7, color='red')
+axs[1, 1].plot(data.index, data['RSI'], label='RSI',
+               linewidth=0.7, color='red')
 axs[1, 1].set_title('RSI')
 axs[1, 1].set_xlabel('日付')
 axs[1, 1].set_ylabel('RSI')
 axs[1, 1].grid()
 
-axs[2, 0].plot(data.index, data['MOM'], label='MOM', linewidth=0.7, color='purple')
+axs[2, 0].plot(data.index, data['MOM'], label='MOM',
+               linewidth=0.7, color='purple')
 axs[2, 0].set_title('MOM')
 axs[2, 0].set_xlabel('日付')
 axs[2, 0].set_ylabel('MOM')
 axs[2, 0].grid()
 
-axs[2, 1].plot(data.index, data['MACD'], label='MACD', linewidth=0.7, color='brown')
+axs[2, 1].plot(data.index, data['MACD'], label='MACD',
+               linewidth=0.7, color='brown')
 axs[2, 1].set_title('MACD')
 axs[2, 1].set_xlabel('日付')
 axs[2, 1].set_ylabel('MACD')
 axs[2, 1].grid()
 
-axs[3, 0].plot(data.index, data['ATR'], label='ATR', linewidth=0.7, color='pink')
+axs[3, 0].plot(data.index, data['ATR'], label='ATR',
+               linewidth=0.7, color='pink')
 axs[3, 0].set_title('ATR')
 axs[3, 0].set_xlabel('日付')
 axs[3, 0].set_ylabel('ATR')
