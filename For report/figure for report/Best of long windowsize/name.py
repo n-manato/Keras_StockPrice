5d5c138b86e54a587478bbd6df798df924704cc6
n-manato <131611846+n-manato@users.noreply.github.com>
2024-02-01 11:34:39 +0900

    20240201

---
+++ For report/figure for report/Best of long windowsize/name.py
@@ -0,0 +1,12 @@
+import os
+
+# フォルダのパス
+folder_path = "./"
+
+# フォルダ内のファイル名を取得
+file_names = os.listdir(folder_path)
+
+# ファイル名を出力
+for file_name in file_names:
+    if file_name != "name.py":
+        print("figure_for_report/Best_of_long_windowsize/{}".format(file_name))
